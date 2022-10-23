68cfc16026845ea3e1b9f294b808a6737acc490e
Nikolay <72947676+svyatoynick@users.noreply.github.com>
2022-10-23 14:49:12 +0300

    Fixed gesture animations.

--- iOS App/Scenes/Scan/ScanController+Gesture.swift
+++ iOS App/Scenes/Scan/ScanController+Gesture.swift
@@ -8,27 +8,48 @@ extension ScanController {
         switch gesture.state {
         case .began:
             initialCenter = scanView.center
+            
         case .changed:
             let translation = gesture.translation(in: view)
             
-            if scanView.center.y > initialCenter.y - 51 {
-                scanView.center = CGPoint(x: initialCenter.x,
-                                          y: initialCenter.y + translation.y)
+            print("TranslationY is \(translation.y)\nsqrt is: \(sqrt(abs(translation.y)))\ninitialcenter is \(initialCenter.y)\nscanView center is \(scanView.center.y)")
+            
+            if translation.y >= 0 {
+                let absTranslationY = abs(translation.y)
+                let sqrtTranslationY = (sqrt(absTranslationY))
+                
+                if sqrtTranslationY < 150 {
+                    scanView.center = CGPoint(x: initialCenter.x,
+                                              y: initialCenter.y + sqrtTranslationY * 7)
+                } else {
+                    scanView.center = CGPoint(x: initialCenter.x,
+                                              y: initialCenter.y + 150 + sqrtTranslationY)
+                }
+                
+                
             } else {
-                scanView.center = CGPoint(x: initialCenter.x,
-                                          y: initialCenter.y - 51)
+                let absTranslationY = abs(translation.y)
+                let sqrtTranslationY = -(sqrt(absTranslationY))
+                if sqrtTranslationY > -100 {
+                    scanView.center = CGPoint(x: initialCenter.x,
+                                              y: initialCenter.y + (sqrtTranslationY * 7))
+                } else {
+                    scanView.center = CGPoint(x: initialCenter.x,
+                                              y: initialCenter.y - 100 + sqrtTranslationY)
+                }
+                
             }
             
             if scanView.center.y < initialCenter.y - 50 {
                 view.backgroundColor = UIColor.black.alpha(0.6)
             } else {
-                if (translation.y / initialCenter.y) < 0.6 {
-                    view.backgroundColor = UIColor.black.alpha(0.6 - (translation.y / initialCenter.y))
+                if (translation.y / scanView.center.y) < 0.6 {
+                    view.backgroundColor = UIColor.black.alpha(0.6 - (translation.y / scanView.center.y))
                     if view.backgroundColor!.alpha > 0.6 {
                         view.backgroundColor = UIColor.black.alpha(0.6)
                     }
                 } else {
-                    if (translation.y / initialCenter.y) > 0.5 {
+                    if (translation.y / scanView.center.y) > 0.5 {
                         view.backgroundColor = UIColor.black.alpha(0.0)
                     } else {
                         view.backgroundColor = UIColor.black.alpha(0.6)
