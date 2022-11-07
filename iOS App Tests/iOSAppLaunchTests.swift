11471889c31a415deb46a4e1d0e44c87fdbb08ce
Ivan Vorobei <hello@ivanvorobei.io>
2022-11-07 17:17:02 +0300

    Added test target.

---
+++ iOS App Tests/iOSAppLaunchTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+final class iOSAppLaunchTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
